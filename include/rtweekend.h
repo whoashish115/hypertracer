60d26e97865f975c36f6c1d6d5563700c153a6e5
Ashish Kumar <who.ashish115@gmail.com>
2026-07-30 15:54:32 +0530

    feat: interval type for the ray bounds

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -24,6 +24,7 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
+#include "interval.h"
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
