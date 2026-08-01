d153e9638a4c3d2e07ea41f11aadf6bab325e57f
Ashish Kumar <who.ashish115@gmail.com>
2026-08-01 10:38:32 +0530

    feat: diffuse bounce and gamma correction

--- include/vec3.h
+++ include/vec3.h
@@ -119,5 +119,20 @@ inline vec3 unit_vector(const vec3 &v)
 {
     return v / v.length();
 }
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        auto lensq = p.length_squared();
+         if (1e-160 < lensq && lensq <= 1)
+            return p / sqrt(lensq);
+    }
+}
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) 
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
 
 #endif
