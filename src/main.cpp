05268440847ac6ef750ce6bd6b56e1d72adc1a23
Ashish Kumar <who.ashish115@gmail.com>
2026-08-02 19:01:51 +0530

    feat: glass

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     auto ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto left = make_shared<dielectric>(1.50);
     auto right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, ground));
