c896588b3e6dc14f5502719889a105f471d7de79
Ashish Kumar <who.ashish115@gmail.com>
2026-08-02 16:23:21 +0530

    feat: lambertian and metal materials

--- include/sphere.h
+++ include/sphere.h
@@ -5,7 +5,8 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -30,12 +31,14 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
         return true;
     }
 
   private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 int main() {
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    auto ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, ground));
+    world.add(make_shared<sphere>(point3( 0.0, 0.0, -1.2), 0.5, center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, left));
+    world.add(make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, right));
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
