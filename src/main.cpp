5bc592d07ad4527fc532ee4b7172243e29cb6273
Ashish Kumar <who.ashish115@gmail.com>
2026-08-03 17:01:08 +0530

    feat: the big random sphere scene

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,35 @@
 int main() {
     hittable_list world;
 
-    auto ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto left = make_shared<dielectric>(1.50);
-    auto right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, ground));
-    world.add(make_shared<sphere>(point3( 0.0, 0.0, -1.2), 0.5, center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, left));
-    world.add(make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, right));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> mat;
+
+                if (choose_mat < 0.8) {
+                    mat = make_shared<lambertian>(color::random() * color::random());
+                } else if (choose_mat < 0.95) {
+                    mat = make_shared<metal>(color::random(0.5, 1), random_double(0, 0.5));
+                } else {
+                    mat = make_shared<dielectric>(1.5);
+                }
+
+                world.add(make_shared<sphere>(center, 0.2, mat));
+            }
+        }
+    }
+
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, make_shared<dielectric>(1.5)));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0,
+                                  make_shared<lambertian>(color(0.4, 0.2, 0.1))));
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0,
+                                  make_shared<metal>(color(0.7, 0.6, 0.5), 0.0)));
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
@@ -25,12 +45,12 @@ int main() {
     cam.max_depth = 50;
 
     cam.vfov = 20;
-    cam.lookfrom = point3(-2, 2, 1);
-    cam.lookat = point3(0, 0, -1);
-    cam.vup = vec3(0, 1, 0);
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat = point3(0,0,0);
+    cam.vup = vec3(0,1,0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     cam.render(world);
 }
